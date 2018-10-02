ff40e9c10f14d349d2c470a49e2dd082a2ecacec
sgroth <stefangroth@me.com>
2018-10-02 10:19:29 +0200

    updated yaml data files

--- generate.sh
+++ generate.sh
@@ -28,6 +28,7 @@ ruby generate.rb -t cv.tex.erb			# cv.de.tex
 # ruby generate.rb -t cv.tex.erb -w -e	# cv.web.en.tex
 # ruby generate.rb -t cv.tex.erb -e		# cv.en.tex
 ruby generate.rb -t presentations.tex.erb		# presentations.de.tex
+ruby generate.rb -t presentations.md.erb		# 
 # web.md version are copied to cv-data by generate.rb
 # switch to output directory
 cd output
