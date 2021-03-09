09f8c078287828c6348eb4a9959ff531a0f4c732
sgroth <stefangroth@me.com>
2021-03-09 20:34:53 +0100

    updated yaml data files

--- gfi-run.py
+++ gfi-run.py
@@ -50,17 +50,16 @@ if len(sys.argv) <= 1:
                       git.log('-1', fl, pretty='format:"%ae"').split('"')[1]]
             commit = [git.log('-1', fl, pretty='format:"%H"').split('"')[1],
                       git.log('-1', fl, pretty='format:"%h"').split('"')[1]]
-            f = codecs.open("cv.web.de.gfi", "w", encoding="utf-8")
+            f = codecs.open("cv.web.de.gfi", "w")
+            # was f = codecs.open("cv.web.de.gfi", "w", encoding="utf-8")
             f.write("% gitfile-info control file\n")
-            f.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]
-                    + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+            f.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]  + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
             f.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
             f.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
             f.close
             g = codecs.open("cv.de.gfi", "w", encoding="utf-8")
             g.write("% gitfile-info control file\n")
-            g.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]
-                    + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+            g.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3] + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
             g.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
             g.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
             g.close
@@ -84,7 +83,7 @@ else:
     fl = sys.argv[1]
     flname, flext = os.path.splitext(fl)
     rawdate = int(git.log('-1', fl, pretty='format:"%at"').split('"')[1])
-    date = [time.strftime("%d. %B %Y %H:%M", time.localtime(rawdate)),
+    date = [time.strftime("%d. %m %Y %H:%M", time.localtime(rawdate)),
             time.strftime("%d", time.localtime(rawdate)),
             time.strftime("%m", time.localtime(rawdate)),
             time.strftime("%Y", time.localtime(rawdate)),
