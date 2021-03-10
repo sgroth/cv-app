29e5761f4f0c871977ed6104c3a425fc0af8d707
sgroth <stefangroth@me.com>
2021-03-10 06:51:51 +0100

    updated yaml data files

--- gfi-run.py
+++ gfi-run.py
@@ -40,8 +40,7 @@ if len(sys.argv) <= 1:
         if fl == 'cv.de.yaml':
             rawdate = int(git.log('-1', fl,
                         pretty='format:"%at"').split('"')[1])
-            date = [time.strftime("%d. %B %Y %H:%M", time.localtime(rawdate)),
-                    time.strftime("%d", time.localtime(rawdate)),
+            date = [time.strftime("%d", time.localtime(rawdate)),
                     time.strftime("%m", time.localtime(rawdate)),
                     time.strftime("%Y", time.localtime(rawdate)),
                     time.strftime("%H", time.localtime(rawdate)),
@@ -50,30 +49,28 @@ if len(sys.argv) <= 1:
                       git.log('-1', fl, pretty='format:"%ae"').split('"')[1]]
             commit = [git.log('-1', fl, pretty='format:"%H"').split('"')[1],
                       git.log('-1', fl, pretty='format:"%h"').split('"')[1]]
-            f = codecs.open("cv.web.de.gfi", "w")
+            f = codecs.open("cv.web.de.gfi", "w", encoding="utf-8")
             # was f = codecs.open("cv.web.de.gfi", "w", encoding="utf-8")
             f.write("% gitfile-info control file\n")
-            f.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]  + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+            f.write("\\gfiSetDate{" + date[0] + "}{" + date[1] + "}{" + date[2]  + "}{" + date[3] + "}{" + date[4] + "}\n")
             f.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
             f.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
             f.close
             g = codecs.open("cv.de.gfi", "w", encoding="utf-8")
             g.write("% gitfile-info control file\n")
-            g.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3] + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+            g.write("\\gfiSetDate{" + date[0] + "}{" + date[1] + "}{" + date[2] + "}{" + date[3] + "}{" + date[4] + "}\n")
             g.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
             g.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
             g.close
             h = codecs.open("cv.web.en.gfi", "w", encoding="utf-8")
             h.write("% gitfile-info control file\n")
-            h.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]
-                   + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+            h.write("\\gfiSetDate{" + date[0] + "}{" + date[1] + "}{" + date[2] + "}{" + date[3] + "}{" + date[4] + "}\n")
             h.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
             h.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
             h.close
             i = codecs.open("cv.en.gfi", "w", encoding="utf-8")
             i.write("% gitfile-info control file\n")
-            i.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]
-                   + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+            i.write("\\gfiSetDate{" + date[0] + "}{" + date[1] + "}{" + date[2] + "}{" + date[3] + "}{" + date[4] + "}\n")
             i.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
             i.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
             i.close
@@ -83,8 +80,7 @@ else:
     fl = sys.argv[1]
     flname, flext = os.path.splitext(fl)
     rawdate = int(git.log('-1', fl, pretty='format:"%at"').split('"')[1])
-    date = [time.strftime("%d. %m %Y %H:%M", time.localtime(rawdate)),
-            time.strftime("%d", time.localtime(rawdate)),
+    date = [time.strftime("%d", time.localtime(rawdate)),
             time.strftime("%m", time.localtime(rawdate)),
             time.strftime("%Y", time.localtime(rawdate)),
             time.strftime("%H", time.localtime(rawdate)),
@@ -95,8 +91,7 @@ else:
               git.log('-1', fl, pretty='format:"%h"').split('"')[1]]
     f = codecs.open("output/"+flname+".gfi", "w", encoding="utf-8")
     f.write("% gitfile-info control file\n")
-    f.write("\\gfiSetDate{" + date[1] + "}{" + date[2] + "}{" + date[3]
-            + "}{" + date[4] + "}{" + date[5] + "}{" + date[0] + "}\n")
+    f.write("\\gfiSetDate{" + date[0] + "}{" + date[1] + "}{" + date[2] + "}{" + date[3] + "}{" + date[4] + "}\n")
     f.write("\\gfiSetAuthor{" + author[0] + "}{" + author[1] + "}\n")
     f.write("\\gfiSetCommit{" + commit[0] + "}{" + commit[1] + "}")
     f.close
